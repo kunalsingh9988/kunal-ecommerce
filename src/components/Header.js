4ce6ef42094fdc92ee767ea90d0a8bf590a37101
Kunal Singh <max42078@.com>
2023-07-27 11:10:53 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <MainHeader>
+      <NavLink to="/">
+        {/* <img src="./images/logo.png" alt="my logo img" /> */}
+        <h1 className="heading">Kunal Store</h1>
+      </NavLink>
+      <Nav />
+    </MainHeader>
+  );
+};
+
+const MainHeader = styled.header`
+  padding: 0 4.8rem;
+  height: 10rem;
+  background-color: ${({ theme }) => theme.colors.bg};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+
+  .heading{
+    font-family: 'Handjet', cursive;
+    color: rgb(63, 54, 156);
+  }
+  .logo {
+    height: 5rem;
+  }
+`;
+export default Header;
